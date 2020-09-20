8b7570276559125efc50582c6a397f26ec609b3b
Abhijit Bhatt <Abhijit.Bhatt@gmail.com>
2020-09-20 14:50:07 -0400

    Testing

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+   
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+        float yAxis = 0.0f;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            yAxis = -1;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            yAxis = 1;
+        }
+
+        transform.position = new Vector3(transform.position.x + xAxis, transform.position.y + yAxis, transform.position.z + zAxis);
+    }
+}
