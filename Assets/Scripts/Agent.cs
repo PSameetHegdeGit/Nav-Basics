f61901d68b3bf64c3e031fc428de480dac6ca208
PSameetHegdeGit <sameethegde123@gmail.com>
2020-09-21 11:00:42 -0400

    got agents to stop once priority 1 object reachs target

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -13,6 +13,7 @@ public class Agent : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        print(Pause.setAgentsVelocity);
     }
 
     // Update is called once per frame
@@ -25,9 +26,25 @@ public class Agent : MonoBehaviour
 
             if(Physics.Raycast(toClickedPoint, out infoForRay, 100, clickable))
             {
-                agent.SetDestination(infoForRay.point);
+                Vector3 agentDestination = infoForRay.point + Random.insideUnitSphere * 0.5f;
+                agentDestination.y = 0;
+                print(agentDestination);
+                agent.SetDestination(agentDestination);
+           
             }
             
+        }
+
+        if (Pause.setAgentsVelocity == 0)
+        {
+            if (agent.velocity == Vector3.zero)
+            {
+                Pause.setAgentsVelocity = 1;
+            }
+        }
+        else if (Pause.setAgentsVelocity == 1)
+        {
+            agent.velocity = Vector3.zero;
         }
     }
 }
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    static public int setAgentsVelocity = 0;
+}
