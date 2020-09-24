01baaf95260f67abb78a027dd379857ae60b02a5
PSameetHegdeGit <sameethegde123@gmail.com>
2020-09-24 17:34:45 -0400

    selecting objects and making only those move works now

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -16,37 +16,7 @@ public class Agent : MonoBehaviour
         print(Pause.setAgentsVelocity);
     }
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            Ray toClickedPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit infoForRay;
-
-            Pause.setAgentsVelocity = 0;
-
-            if(Physics.Raycast(toClickedPoint, out infoForRay, 100, clickable))
-            {
-                Vector3 agentDestination = infoForRay.point + Random.insideUnitSphere * 0.5f;
-                agentDestination.y = 0;
-                print(agentDestination);
-                agent.SetDestination(agentDestination);
-           
-            }
-            
-        }
-
-        if (Pause.setAgentsVelocity == 0)
-        {
-            if (agent.velocity == Vector3.zero)
-            {
-                Pause.setAgentsVelocity = 1;
-            }
-        }
-        else if (Pause.setAgentsVelocity == 1)
-        {
-            agent.velocity = Vector3.zero;
-        }
-    }
+    
 }
+
+
--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -6,4 +6,9 @@ public class Pause : MonoBehaviour
 {
 
     static public int setAgentsVelocity = 0;
+
+
+
+
+
 }
