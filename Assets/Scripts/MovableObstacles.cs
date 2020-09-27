bce69360a8721a3e8a37271a337495fdd8d64c6e
PSameetHegdeGit <sameethegde123@gmail.com>
2020-09-26 20:28:43 -0400

    modified movable objects scripts

--- Assets/Scripts/MovableObstacles.cs
+++ Assets/Scripts/MovableObstacles.cs
@@ -4,20 +4,81 @@ using UnityEngine;
 
 public class MovableObstacles : MonoBehaviour
 {
+
+    public string selectableTag = "movableObject";
+    private Transform movableObject;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        movableObject = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var movement = Camera.main.transform.right * Input.GetAxis("Horizontal_Obst");
-        var verticalMovement = Input.GetAxis("Vertical_Obst");
-        var temp = Camera.main.transform.forward;
-        temp.y = 0;
-        movement += temp.normalized * verticalMovement;
-        transform.position += movement / 10;
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray clickedPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(clickedPoint, out hit))
+            {
+                var selection = hit.transform;
+
+                if (selection.CompareTag(selectableTag))
+                {
+                    if (selection.GetComponent<Renderer>().material.color != Color.red)
+                    {
+                        Select(selection);
+                    }
+                    else
+                    {
+                        Deselect(selection);
+                    }
+
+
+                }
+
+            }
+
+        }
+
+        if (movableObject != null)
+        {
+            var movement = Camera.main.transform.right * Input.GetAxis("Horizontal_Obst");
+            var verticalMovement = Input.GetAxis("Vertical_Obst");
+            var temp = Camera.main.transform.forward;
+            temp.y = 0;
+            movement += temp.normalized * verticalMovement;
+            movableObject.position += movement/4;
+        }
+
+      
+    }
+
+
+
+
+    void Select(Transform selection)
+    {
+        print("In Select!");
+        if (movableObject != null)
+        {
+            movableObject.GetComponent<Renderer>().material.color = Color.yellow;
+            movableObject = null;
+        }
+
+        movableObject = selection.GetComponent<Transform>();
+        selection.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void Deselect(Transform selection)
+    {
+        print("In deselect!");
+        selection.GetComponent<Renderer>().material.color = Color.yellow;
+        movableObject = null;
+
     }
 }
